c4d2a626986815565433c4a87d8879f312c1e745
JhonUsuga <jhon.usuga1@udea.edu.co>
2025-06-24 17:54:20 -0500

    Reibe notificaciones y puede abrir la aplicacion directamente desde la notificacion

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr07_20251/lab2/worker/SyncEpisodesWorker.kt
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr07_20251/lab2/worker/SyncEpisodesWorker.kt
@@ -13,6 +13,9 @@ import co.edu.udea.compumovil.gr07_20251.lab2.data.network.fetchEpisodesFromRss
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import co.edu.udea.compumovil.gr07_20251.lab2.R
+import android.app.PendingIntent
+import android.content.Intent
+import co.edu.udea.compumovil.gr07_20251.lab2.MainActivity
 
 class SyncEpisodesWorker(
     appContext: Context,
@@ -49,11 +52,24 @@ private fun showNotification(context: Context, message: String) {
         )
         notificationManager.createNotificationChannel(channel)
     }
+    val intent = Intent(context, MainActivity::class.java).apply {
+        flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+    }
+
+    val pendingIntent = PendingIntent.getActivity(
+        context,
+        0,
+        intent,
+        PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+    )
 
     val notification = NotificationCompat.Builder(context, channelId)
         .setContentTitle(context.getString(R.string.notification_sync_title))
         .setContentText(message)
         .setSmallIcon(android.R.drawable.ic_popup_sync)
+        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        .setAutoCancel(true)
+        .setContentIntent(pendingIntent)
         .build()
 
     notificationManager.notify(1, notification)
